9902b7332e6a238b04117674f46572a26774e529
vetalreal <vetalreal@ukr.net>
2012-10-28 00:16:46 +0300

    Звкончена верстка шаблона в index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ function retArray($param1, $param2, $param3 = 1970) {
   return $arr;
 }
 
-$arr2[] = 'krghkjr tryhy{$year} yhr hyr'; // krghkjr tryhy{$year} yhr hyr
-$arr2[] = "krghkjr tryhy{$year} yhr hyr"; // krghkjr tryhy2012 yhr hyr
+$arr2[] = 'На дворе {$year} год'; // На дворе {$year} год
+$arr2[] = "На дворе {$year} год"; // На дворе 2012 год
 $arr2['serg'] = 24;
 $arr2[] = 125;
 ?>
@@ -21,6 +21,9 @@ $arr2[] = 125;
   <body>
     <div id="wrapper">
       <div id="header">
+        <a href="#" id="logo">
+          <img src="img/asaweb-logo.png" alt="ASA web - start your business" />
+        </a>
         <ul id="common_menu">
           <li>
             <a href="#">Login</a>
@@ -47,9 +50,16 @@ $arr2[] = 125;
             <a href="#">Карта сайта</a>
           </li>
           <li>
-            <a href="#">Цены</a>
+            <a href="#" class="active">Цены</a>
           </li>
         </ul>
+        <div id="site_title">
+          <div class="site_info">
+            <h1>DIGITAL DREAM UTOPIA!</h1>
+            <h2>Start your business</h2>
+            <p class="description">Site about IT</p>
+          </div>
+        </div>
       </div>
       <div id="middle">
         <div id="sidebar">
@@ -70,21 +80,154 @@ $arr2[] = 125;
           </ul>
           <div class="info">
             <img src="img/phone.png" />
-            Service Center
+            <div class="name">Service Center</div>
+            <div class="description">
+              12-34-567
+            </div>
           </div>
           <div class="info">
             <img src="img/calendar.png" />
-            E-catalog
+            <div class="name">E-catalog</div>
+            <div class="description">
+              27.10.2012
+            </div>
           </div>
         </div>
         <div id="content">
           <div class="inner">
-            Основное содержимое страницы 
-            При создании веб-страницы часто приходится вкладывать одни теги внутрь других. Чтобы стили для этих тегов использовались корректно, помогут селекторы, которые работают только в определенном контексте. Например, задать стиль для тега только когда он располагается внутри контейнера <p>. Таким образом можно одновременно установить стиль для отдельного тега, а также для тега, который находится внутри другого.</p>
-            <div class="paging">
-              <input type="text" />
-              <input type="text" />
+            <div class="top">
+              <ul class="bradcrambs">
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="#">Цены</a>
+                </li>
+                <li>
+                  <a href="#">Без НДС</a>
+                </li>
+              </ul>
+              <div class="page_name">
+                Без НДС
+              </div>
+            </div>
+            <div class="title">
 
+            </div>
+            <div class="data">
+              <table>
+                <thead>
+                  <tr>
+                    <th class="numb">№</th>
+                    <th class="name">Наименование товара</th>
+                    <th class="model">Модель</th>
+                    <th class="date">Дата поступления</th>
+                    <th class="count">Кол-во</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="numb">1</td>
+                    <td class="name">Товар 1</td>
+                    <td class="model">Модель 1</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">11</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">2</td>
+                    <td class="name">Товар 2</td>
+                    <td class="model">Модель 2</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">12</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">3</td>
+                    <td class="name">Товар 3</td>
+                    <td class="model">Модель 3</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">13</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">4</td>
+                    <td class="name">Товар 4</td>
+                    <td class="model">Модель 4</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">14</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">5</td>
+                    <td class="name">Товар 5</td>
+                    <td class="model">Модель 5</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">15</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">6</td>
+                    <td class="name">Товар 6</td>
+                    <td class="model">Модель 6</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">16</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">7</td>
+                    <td class="name">Товар 7</td>
+                    <td class="model">Модель 7</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">17</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">8</td>
+                    <td class="name">Товар 8</td>
+                    <td class="model">Модель 8</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">18</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">9</td>
+                    <td class="name">Товар 9</td>
+                    <td class="model">Модель 9</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">19</td>
+                  </tr>
+                  <tr>
+                    <td class="numb">10</td>
+                    <td class="name">Товар 10</td>
+                    <td class="model">Модель 10</td>
+                    <td class="date">27.10.2012</td>
+                    <td class="count">20</td>
+                  </tr>
+                </tbody>
+
+              </table>
+              <div class="paging">
+                <select name="brand">
+                  <option value="brand1">Производитель 1</option>
+                  <option value="brand2">Производитель 2</option>
+                  <option value="brand3" selected="selected">Производитель 3</option>
+                  <option value="brand4">Производитель 4</option>
+                </select>
+                <input type="text" name="keyword" />
+                <input type="submit" value="Поиск" />
+                <input type="submit" value="Обновить" class="refresh" />
+                <div class="pages">
+                  <a class="first" href="#first">&lt;&lt;</a>
+                  <a class="previus" href="#previus">&lt;</a>
+                  <span class="numbers">
+                    <a href="#p1" class="p1 curr">1</a>
+                    <a href="#p2" class="p2">2</a>
+                    <a href="#p3" class="p3">3</a>
+                    <a href="#p4" class="p4">4</a>
+                    <a href="#p5" class="p5">5</a>
+                    <a href="#p6" class="p6">6</a>
+                    <a href="#p7" class="p7">7</a>
+                    <a href="#p8" class="p8">8</a>
+                    <a href="#p9" class="p9">9</a>
+                    <a href="#p10" class="p10">10</a>
+                  </span>
+                  <a class="first" href="#next">&gt;</a>
+                  <a class="previus" href="#last">&gt;&gt;</a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -93,7 +236,7 @@ $arr2[] = 125;
         <pre>
           <?php
           echo $year;
-          $natArr = retArray(23, "erhjbgrh", 0);
+          $natArr = retArray(23, "Второй элемент массива", 0);
           var_dump($arr2);
           ?>
         </pre>
@@ -101,13 +244,14 @@ $arr2[] = 125;
         echo $natArr[2];
 
         unset($arr2[1]);
-        var_dump($arr2);?>
+        var_dump($arr2);
+        ?>
         <ul>
-        <? foreach ($arr2 as $key => $vlad_s_value) {?>
-          <li class="<?= $key;?>"><?= $vlad_s_value; ?></li>
-        <? } ?>
+          <?php foreach ($arr2 as $key => $vlad_s_value) { ?>
+            <li class="<?php echo $key; ?>"><?php echo $vlad_s_value; ?></li>
+          <?php } ?>
         </ul>
-        
+
       </div>
     </div>
   </body>
