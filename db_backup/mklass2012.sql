0d21aaad71d342f2c81f7b6c36e5b6a065e1725f
vetalreal <vetalreal@ukr.net>
2012-12-15 17:27:32 +0200

    Добавлен дамп БД.

--- db_backup/mklass2012.sql
+++ db_backup/mklass2012.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Дек 01 2012 г., 16:28
+-- Время создания: Дек 15 2012 г., 15:26
 -- Версия сервера: 5.5.24-log
 -- Версия PHP: 5.3.13
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `parent_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `parent_id` (`parent_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Дамп данных таблицы `menu`
@@ -41,7 +41,8 @@ CREATE TABLE IF NOT EXISTS `menu` (
 INSERT INTO `menu` (`id`, `name`, `parent_id`) VALUES
 (1, 'Главное меню', NULL),
 (2, 'Меню подвала', NULL),
-(3, 'Контекстное меню', NULL);
+(3, 'Цены', 6),
+(4, 'Контакты', 2);
 
 -- --------------------------------------------------------
 
@@ -52,12 +53,12 @@ INSERT INTO `menu` (`id`, `name`, `parent_id`) VALUES
 CREATE TABLE IF NOT EXISTS `menu_item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `page_id` int(11) NOT NULL,
+  `page_id` int(11) DEFAULT NULL,
   `menu_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `page_id` (`page_id`),
   KEY `menu_id` (`menu_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
 
 --
 -- Дамп данных таблицы `menu_item`
@@ -65,7 +66,14 @@ CREATE TABLE IF NOT EXISTS `menu_item` (
 
 INSERT INTO `menu_item` (`id`, `name`, `page_id`, `menu_id`) VALUES
 (1, 'Главная', 1, 1),
-(2, 'Контакты', 2, 1);
+(2, 'Контакты', 2, 1),
+(3, 'Примеры', 5, 1),
+(4, 'Карта сайта', 6, 1),
+(6, 'Цены', 7, 1),
+(7, 'Оптовые', 1, 3),
+(8, 'Розничные', 2, 3),
+(9, 'Офис 1', 8, 4),
+(10, 'Офис 2', 5, 4);
 
 -- --------------------------------------------------------
 
@@ -81,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `page` (
   `href` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `href` (`href`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
 --
 -- Дамп данных таблицы `page`
@@ -89,7 +97,11 @@ CREATE TABLE IF NOT EXISTS `page` (
 
 INSERT INTO `page` (`id`, `title`, `description`, `content`, `href`) VALUES
 (1, 'Главная страница нашего демонстрационного сайта по курсу веб-программирования', 'Главная страница нашего демонстрационного сайта по курсу веб-программирования', 'Содержимое этой самой главной страницы. <strong> Как хорошо быть</strong> специалистом в IT.', 'index'),
-(2, 'Страница контактов', 'Описание этой страницы контактов', 'г.Харьков, ул.Ленина, д.3, кв.4', 'contacts');
+(2, 'Страница контактов', 'Описание этой страницы контактов', 'г.Харьков, ул.Ленина, д.3, кв.4', 'contacts'),
+(5, 'Примеры работ', NULL, 'Содержимое примеров работ', 'examples'),
+(6, 'Карта сайта', 'Здесь Вы можете увидеть всю структуру сайта', 'Содержимое "Карта сайта"', 'sitemap'),
+(7, 'Наш прайс', 'Наш прайс', 'Каталог цен (проверяйте каждый день, может повезет:)', 'price'),
+(8, 'Офис 1', 'керкерке', 'Контакты в офисе №1', 'office1');
 
 -- --------------------------------------------------------
 
